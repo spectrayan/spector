5b7621579891f9741829bde3a891b24e4f43a03d
Bharat Joshi <bharatjoshi@spectrayan.com>
2026-06-07 14:11:21 -0500

    fix: correct BSL license headers in memory package-info files
    - Add missing 'You may obtain a copy of the License at' URL lines
    - Matches expected format from license-maven-plugin bsl-header.txt template
    - Fixes license:check CI failure for 19 spector-memory package-info files

--- spector-memory/src/main/java/com/spectrayan/spector/memory/metamemory/package-info.java
+++ spector-memory/src/main/java/com/spectrayan/spector/memory/metamemory/package-info.java
@@ -3,11 +3,13 @@
  *
  * Licensed under the Business Source License 1.1 (the "License");
  * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://github.com/spectrayan/spector/blob/main/spector-memory/LICENSE
  *
  * Change Date: May 27, 2030
  * Change License: Apache License, Version 2.0
  */
-
 /**
  * Meta-memory: introspection and self-awareness about memory contents and reliability.
  */
