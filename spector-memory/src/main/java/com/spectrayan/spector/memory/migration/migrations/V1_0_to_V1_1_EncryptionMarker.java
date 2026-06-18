9d9e09e0d554c35fd6dad31e09410d6d198f4092
Bharat Joshi <bharatjoshi@spectrayan.com>
2026-06-17 19:43:37 -0500

    chore: add BSL 1.1 license headers to new spector-memory files

--- spector-memory/src/main/java/com/spectrayan/spector/memory/migration/migrations/V1_0_to_V1_1_EncryptionMarker.java
+++ spector-memory/src/main/java/com/spectrayan/spector/memory/migration/migrations/V1_0_to_V1_1_EncryptionMarker.java
@@ -1,8 +1,14 @@
 /*
  * Copyright 2026 Spectrayan
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Business Source License 1.1 (the "License");
  * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://github.com/spectrayan/spector/blob/main/spector-memory/LICENSE
+ *
+ * Change Date: May 27, 2030
+ * Change License: Apache License, Version 2.0
  */
 package com.spectrayan.spector.memory.migration.migrations;
 
