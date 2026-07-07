12f675b7dabc32757334c4efd52c593cdbe729ba
Bharat Joshi <bharatjoshi@spectrayan.com>
2026-07-06 21:30:46 -0500

    chore(synapse): apply BSL 1.1 license headers to all Java files
    Run license:format to replace Apache 2.0 headers with BSL 1.1
    headers across 110 Java files in spector-synapse (src/main + src/test).

--- spector-synapse/src/main/java/com/spectrayan/spector/synapse/agent/chat/infrastructure/SpectorMemoryChatAdapter.java
+++ spector-synapse/src/main/java/com/spectrayan/spector/synapse/agent/chat/infrastructure/SpectorMemoryChatAdapter.java
@@ -1,17 +1,14 @@
 /*
  * Copyright 2026 Spectrayan
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Business Source License 1.1 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     https://github.com/spectrayan/spector/blob/main/spector-synapse/LICENSE
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Change Date: July 6, 2030
+ * Change License: Apache License, Version 2.0
  */
 package com.spectrayan.spector.synapse.agent.chat.infrastructure;
 
--- spector-synapse/src/test/java/com/spectrayan/spector/synapse/SynapseApplicationTest.java
+++ spector-synapse/src/test/java/com/spectrayan/spector/synapse/SynapseApplicationTest.java
@@ -1,5 +1,14 @@
 /*
- * Copyright 2025–2026 Spectrayan. Licensed under the Apache License, Version 2.0.
+ * Copyright 2026 Spectrayan
+ *
+ * Licensed under the Business Source License 1.1 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://github.com/spectrayan/spector/blob/main/spector-synapse/LICENSE
+ *
+ * Change Date: July 6, 2030
+ * Change License: Apache License, Version 2.0
  */
 package com.spectrayan.spector.synapse;
 
